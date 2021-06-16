97f2c1c2ff0ee2b7a9a6539abc7549de2076410b
Mayur Ingle <inglemayur8890@gmail.com>
2021-06-16 11:23:59 +0530

    Solution of Hacker rank Java Introduction

---
+++ 3]. Competitive Programming/HackerRank/Java/introduction/JavaIntToString.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.javaaid.hackerrank.solutions.languages.java.introduction;
+
+/**
+ * @author Mayur Ingle(https://github.com/thecoder8890)
+ * https://www.hackerrank.com/challenges/java-int-to-string/problem
+ *
+ */
+public class JavaIntToString {
+	int n; //already decalared
+	String s=n+"";
+}
