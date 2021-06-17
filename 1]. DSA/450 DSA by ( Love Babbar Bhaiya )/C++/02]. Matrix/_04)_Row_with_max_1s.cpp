6f39852a4642125c4c067d0df2b60577ce7c4dca
Raunak <raunakpandey1007@gmail.com>
2021-06-17 12:03:41 +0530

    _04)_Row_with_max_1s using C++

---
+++ 1]. DSA/450 DSA by ( Love Babbar Bhaiya )/C++/02]. Matrix/_04)_Row_with_max_1s.cpp
@@ -0,0 +1,61 @@
+//Link: https://practice.geeksforgeeks.org/problems/row-with-max-1s0023/1
+/*
+Given a boolean 2D array of n x m dimensions where each row is sorted. Find the 0-based index of the first row that has the maximum number of 1's.
+
+Example 1:
+
+Input: 
+N = 4 , M = 4
+Arr[][] = {{0, 1, 1, 1},
+           {0, 0, 1, 1},
+           {1, 1, 1, 1},
+           {0, 0, 0, 0}}
+Output: 2
+Explanation: Row 2 contains 4 1's (0-based
+indexing).*/
+#include <bits/stdc++.h>
+using namespace std;
+
+  
+//User function template for C++
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    // code here
+	    int maxRow=-1;
+	    int i=0,j=m-1;
+	    while(i<n && j>=0)
+	    {
+	        if(arr[i][j]==1)
+	        {
+	            maxRow=i;
+	            j--;
+	        }
+	        else
+	            i++;
+	    }
+	    return maxRow;
+	}
+
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector< vector<int> > arr(n,vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin>>arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
