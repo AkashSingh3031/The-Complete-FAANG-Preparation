cea23c4710b5c18cf6a4b432c4162e7f4e0ff3ef
Shubhankar Poddar <65807708+codeaholic-shub@users.noreply.github.com>
2021-06-17 09:08:41 +0530

    Update SHORTEST_ROUTE.cpp

--- 3]. Competitive Programming/CodeChef/JUNE_LONG_CHALLENGE_DIV_3/SHORTEST_ROUTE.cpp
+++ 3]. Competitive Programming/CodeChef/JUNE_LONG_CHALLENGE_DIV_3/SHORTEST_ROUTE.cpp
@@ -1,124 +1,78 @@
-
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-	// your code goes here
-	int t;
-	scanf("%d",&t);
-	while(t--){
-	    int n,m;
-	    scanf("%d%d",&n,&m);
-	    int a[n];
-	    int l[n];
-	    int r[n];
-	    int temposl=-1;
-	    int temposr=-1;
-	    int posl[n];
-	    int posr[n];
-	    int templ=INT_MAX;
-	    int tempr = INT_MIN;
-	    for(int i=0;i<n;i++)
-	    {
-	       scanf("%d",&a[i]);
-	    }
-	    for(int i=0;i<n;i++)
-	    {
-	        if(a[i]!=0){
-	            if(a[i]<=templ)
-	            {
-	                templ=a[i];
-	                l[i]=templ;
-	                temposl =i;
-	                posl[i]=temposl;
-	            }
-	             else{
-	            l[i]=templ;
-	            posl[i]=temposl;
-	        }
-	        }
-	        else{
-	            l[i]=templ;
-	            posl[i]=temposl;
-	        }
-	    }
-	   // for(int i =0;i<n;i++)
-	   // {
-	   //     cout<<l[i]<<" ";
-	   // }
-	   // cout<<endl;
-	   //  for(int i =0;i<n;i++)
-	   // {
-	   //     cout<<posl[i]<<" ";
-	   // }
-	   //  cout<<endl;
-
-	    
-	    
-	    for(int i=n-1;i>=0;i--){
-	        if(a[i]!=0){
-	            if(a[i]>=tempr)
-	            {
-	                tempr=a[i];
-	                r[i]=tempr;
-	                temposr=i;
-	                posr[i]=temposr;
-	            }
-	              else{
-	            r[i]=tempr;
-	            posr[i]=temposr;
-	        }
-	        }
-	        else{
-	            r[i]=tempr;
-	            posr[i]=temposr;
-	        }
-	    }
-	  
-	   //	     for(int i =0;i<n;i++)
-	   // {
-	   //     cout<<r[i]<<" ";
-	   // }
-	   //  cout<<endl;
-	   //  for(int i =0;i<n;i++)
-	   // {
-	   //     cout<<posr[i]<<" ";
-	   // }
-	   //  cout<<endl;
-	
-	
-	    for(int i=0;i<m;i++)
-	    {
-	        int q;
-	        scanf("%d",&q);
-	        q=q-1;
-if(q==0)
+#define int long long
+#define MOD 1000000007
+void solve()
 {
-printf("%d ",0);
-continue;
+int i;
+int n, m;
+cin >> n >> m;
+int a[n];
+int b[m];
+for (i = 0; i < n; i++)
+{
+cin >> a[i];
 }
-	        if(a[q]!=0)
-	       { printf("%d ",0);}
-	       else{
-	        int left = l[q];
-	        int right = r[q];
-	        if(left==1&&right==2)
-	        {
-	           printf("%d ",min(q-posl[q],posr[q]-q));
-	        }
-	        else if(left==1&&right!=2){
-	    printf("%d ",(q-posl[q]));
-	        }
-	        else if(right==2&&left!=1){
-	           printf("%d ",(posr[q]-q));
-	        }
-	        else
-	     
-	        printf("%d ",-1);
-	       }
-	    }
-	   printf("\n");
-	}
-	return 0;
+for (i = 0; i < m; i++)
+{
+cin >> b[i];
+}
+int max = 1e9;
+map<int, int> mp;
+for (i = 0; i < n; i++)
+{
+if (i == 0)
+mp[i] = 0;
+else if (a[i] != 0)
+mp[i] = 0;
+else
+mp[i] = max;
+}
+int right = -1;
+for (i = 0; i < n; i++)
+{
+if (a[i] == 1)
+right = i;
+if (right != -1)
+{
+if (a[i] == 0)
+mp[i] = min(mp[i], i - right);
+}
+}
+int left = -1;
+for (i = n - 1; i >= 0; i--)
+{
+if (a[i] == 2)
+left = i;
+if (left != -1)
+{
+if (a[i] == 0)
+mp[i] = min(mp[i], left - i);
+}
+}
+for (i = 0; i < m; i++)
+{
+int j = b[i] - 1;
+if (mp[j] != max)
+cout << mp[j] << " ";
+else
+cout << -1 << " ";
+}
+cout << "\n";
+}
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+int t;
+cin >> t;
+// t = 1;
+int _ = 1;
+while (t--)
+{
+// cout<<"Case #"<<_++<<": ";
+solve();
+}
+return 0;
 }
