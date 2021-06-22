1c9e78f3f916c451da309ea430a311d8f04591f4
Aditya Tyagi <77538183+adityagi02@users.noreply.github.com>
2021-06-22 15:11:22 +0530

    Create Hello_World.c

---
+++ 3]. Competitive Programming/HackerRank/C/Hello_World.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+	
+ { char name[1000],i;
+   gets(name);
+  printf("Hello, World!\n");
+  puts(name);
+     
+ }
