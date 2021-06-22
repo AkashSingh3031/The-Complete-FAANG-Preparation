2b78e1c9a5bd5b32b5c58e4c1f5d6a524f6be5da
Vedant Vyas <53002423+vedudx@users.noreply.github.com>
2021-06-22 14:48:09 +0530

    Update 0014)Longest_common_prefix.py

--- 3]. Competitive Programming/LeetCode/Problems/Python/0014)Longest_common_prefix.py
+++ 3]. Competitive Programming/LeetCode/Problems/Python/0014)Longest_common_prefix.py
@@ -1,5 +1,7 @@
 class Solution:
-    
+    #strs is a list of strings
+    #mid is the endpos of substring to be checked
+    #returns True if strs[0: mid] is a common prefix, false otherwise
     def commonPre(self, strs, mid):
         comp = strs[0][:mid]
         for i in range(1, len(strs)):
@@ -7,12 +9,16 @@ class Solution:
                 return False
         return True
     
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    #strs is a list of str
+    #returns str which is the largest common prefix
+    def longestCommonPrefix(self, strs):
         minLen = 1000000
+        #looking for minLen from all strings in the list as the common prefix can be at max the full length as the shortest string
         for a in strs:
             minLen = min(minLen, len(a))
         lo, hi = 1, minLen
-
+        
+        #using binary search to find the common prefix
         while lo <= hi:
             mid = (lo + hi) //2
             if self.commonPre(strs, mid):
