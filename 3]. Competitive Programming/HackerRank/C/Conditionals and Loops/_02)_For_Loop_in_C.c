2f616e36df39ca666110255967749594b01fda49
Aditya Tyagi <77538183+adityagi02@users.noreply.github.com>
2021-06-26 16:17:25 +0530

    Update _02)_For_Loop_in_C.c

--- 3]. Competitive Programming/HackerRank/C/Conditionals and Loops/_02)_For_Loop_in_C.c
+++ 3]. Competitive Programming/HackerRank/C/Conditionals and Loops/_02)_For_Loop_in_C.c
@@ -9,36 +9,55 @@ int main()
 {
     int a, b,i,j;
     scanf("%d\n%d", &a, &b);
- for(i=a;i<=b;i++){
-    if(i==1){
+ for(i=a;i<=b;i++)
+ {
+    if(i==1)
+    {
     printf("one\n");
-    }else if (i==2) {
+    }
+     else if (i==2) 
+    {
     printf("two\n");
-    }else if (i==3) {
+    }
+     else if (i==3) 
+    {
     printf("three\n");
-    }else if (i==4) {
+    }
+     else if (i==4) 
+    {
     printf("four\n");
-    }else if (i==5) {
+    }
+     else if (i==5) 
+    {
     printf("five\n");
-    }else if (i==6) {
+    }
+     else if (i==6) 
+    {
     printf("six\n");
-    }else if (i==7) {
+    }
+     else if (i==7) 
+    {
     printf("seven\n");
-    }else if (i==8) {
+    }
+     else if (i==8) 
+    {
     printf("eight\n");
-    }else if (i==9) {
+    }
+     else if (i==9) 
+    {
     printf("nine\n");
     }
  }
      for(j=10;j<=b;j++){
           if(j%2==0)
-           {
+            {
               printf("even\n");
-           }
-          else{
+              }
+          else
+            {
             printf("odd\n");
-               }
-     };
+              }
+     }; 
   return 0;
 }
 
