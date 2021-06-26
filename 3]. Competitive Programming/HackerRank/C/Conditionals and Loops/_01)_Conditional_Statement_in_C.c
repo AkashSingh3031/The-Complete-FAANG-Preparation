4c674149af21d672dd30bed064b4332b02b10bfd
Aditya Tyagi <77538183+adityagi02@users.noreply.github.com>
2021-06-26 16:15:50 +0530

    Update _01)_Conditional_Statement_in_C.c

--- 3]. Competitive Programming/HackerRank/C/Conditionals and Loops/_01)_Conditional_Statement_in_C.c
+++ 3]. Competitive Programming/HackerRank/C/Conditionals and Loops/_01)_Conditional_Statement_in_C.c
@@ -15,23 +15,40 @@ int main()
     int i;
     scanf("%d",&i);
       
-     if(i==1){
+     if(i==1)
+     {
        printf("one");
-    }else if (i==2) {
+    }
+    else if (i==2) 
+    {
        printf("two");
-    }else if (i==3) {
+    }
+    else if (i==3)
+    {
        printf("three");
-    }else if (i==4) {
+    }
+    else if (i==4) 
+    {
        printf("four");
-    }else if (i==5) {
+    }
+    else if (i==5) 
+    {
        printf("five");
-    }else if (i==6) {
+    }
+    else if (i==6) 
+    {
        printf("six");
-    }else if (i==7) {
+    }
+    else if (i==7) 
+    {
        printf("seven");
-    }else if (i==8) {
+    }
+    else if (i==8) 
+    {
        printf("eight");
-    }else if (i==9) {
+    }
+    else if (i==9) 
+    {
        printf("nine");
     }
     else{
