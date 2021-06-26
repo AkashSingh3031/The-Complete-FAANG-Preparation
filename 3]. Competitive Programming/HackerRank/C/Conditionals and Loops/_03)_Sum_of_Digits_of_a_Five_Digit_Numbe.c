f889945a2145c245a56aec14d20a1171bc68b0c6
Aditya Tyagi <77538183+adityagi02@users.noreply.github.com>
2021-06-26 16:11:47 +0530

    Update _03)_Sum_of_Digits_of_a_Five_Digit_Numbe.c

--- 3]. Competitive Programming/HackerRank/C/Conditionals and Loops/_03)_Sum_of_Digits_of_a_Five_Digit_Numbe.c
+++ 3]. Competitive Programming/HackerRank/C/Conditionals and Loops/_03)_Sum_of_Digits_of_a_Five_Digit_Numbe.c
@@ -6,12 +6,14 @@
 int main() {
 	
     int n,rem=0,sum=0;
-    scanf("%d", &n);
-   while(n!=0){
-       rem=n%10;
-       sum=sum+rem;
-       n=n/10;}
-   printf("%d",sum);
-    return 0;
+    scanf("%d", &n);	
+    while(n!=0)
+       {
+         rem=n%10;
+         sum=sum+rem;
+         n=n/10;
+    }
+    printf("%d",sum);
+  return 0;
 }
 
