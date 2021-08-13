a2a1deab3312b65dbb62d1592d55fcbc72b2e2a2
Soumen Saha <43337272+imsoumen@users.noreply.github.com>
2021-08-13 22:13:43 +0530

    Adding Scala & Bash solution for 30 Days of Code
    Soumen

--- 3]. Competitive Programming/03]. HackerRank/1]. Practice/08]. 30 Days of Code/Scala/Day_10_Binary_Numbers.scala
+++ 3]. Competitive Programming/03]. HackerRank/1]. Practice/08]. 30 Days of Code/Scala/Day_10_Binary_Numbers.scala
@@ -45,6 +45,26 @@ The binary representation of 13 base-10 is 1101 base-2, so the maximum number of
 
 //(Scala Solution)
 
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+import scala.collection.immutable._
+import scala.collection.mutable._
+import scala.collection.concurrent._
+import scala.concurrent._
+import scala.io._
+import scala.io.StdIn._
+import scala.math._
+import scala.sys._
+import scala.util.matching._
+import scala.reflect._
+
 object Solution {
     def main(args: Array[String]) {
         val n = readLine.trim.toInt
