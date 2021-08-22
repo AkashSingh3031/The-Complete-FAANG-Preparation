271838e990af12bbcc4271e5522cfd6914823f01
Akash Singh <60146338+AkashSingh3031@users.noreply.github.com>
2021-08-22 15:41:43 +0530

    code update

--- 3]. Competitive Programming/04]. GeeksforGeeks/3]. Must Do Coding Questions/02]. Array/1]. Easy/C++/Rotate_Array.cpp
+++ 3]. Competitive Programming/04]. GeeksforGeeks/3]. Must Do Coding Questions/02]. Array/1]. Easy/C++/Rotate_Array.cpp
@@ -25,7 +25,7 @@ int main()
 	//taking testcases
 	cin >> t;
 	while(t--)
-  {
+  	{
 	    int n, d;	    
 	    //input n and d
 	    cin >> n >> d;    
@@ -33,19 +33,16 @@ int main()
     
 	    //inserting elements in the array
 	    for(int i = 0; i < n; i++)
-      {
-	        cin >> arr[i];
-	    }
+      	    	cin >> arr[i];
+	    
 	    Solution ob;
-    
 	    //calling rotateArr() function
 	    ob.rotateArr(arr, d,n);
     
 	    //printing the elements of the array
 	    for(int i =0;i<n;i++)
-      {
-	        cout << arr[i] << " ";
-	    }
+      		cout << arr[i] << " ";
+	    
 	    cout << endl;
 	}
 	return 0;
