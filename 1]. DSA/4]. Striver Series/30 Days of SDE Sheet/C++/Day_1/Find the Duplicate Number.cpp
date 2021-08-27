4d78e96d0d3f49a3969da18007e373f35800cff6
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-08-27 21:47:19 +0530

    Create Find the Duplicate Number.cpp

---
+++ 1]. DSA/4]. Striver Series/30 Days of SDE Sheet/C++/Day_1/Find the Duplicate Number.cpp
@@ -0,0 +1,21 @@
+int findDuplicate(vector<int>& nums) 
+    {
+        if(nums.size() <= 1)
+            return -1;
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        fast = 0;
+        while (fast != slow)
+        {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+
+    }
