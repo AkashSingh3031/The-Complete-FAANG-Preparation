27c9385b8a644e31d8df2dc61ac0d0f8b56b84e9
Akash Singh <60146338+AkashSingh3031@users.noreply.github.com>
2021-08-28 20:44:59 +0530

    update

--- 1]. DSA/4]. Striver Series/30 Days of SDE Sheet/C++/Day-02 (Arrays)/2].Pascal's Triangle.cpp
+++ 1]. DSA/4]. Striver Series/30 Days of SDE Sheet/C++/Day-02 (Arrays)/2].Pascal's Triangle.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <bits/stdc++.h>
 using namespace std;
 
