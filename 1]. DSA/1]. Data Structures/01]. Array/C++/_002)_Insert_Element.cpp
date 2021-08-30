e8cd9107a936f6c559b38ac4fa568cb6709b29f3
harshil202 <83488223+harshil202@users.noreply.github.com>
2021-08-30 12:29:49 +0530

    Add comments

--- 1]. DSA/1]. Data Structures/01]. Array/C++/_002)_Insert_Element.cpp
+++ 1]. DSA/1]. Data Structures/01]. Array/C++/_002)_Insert_Element.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+//This function will insert element in array
 int insert(int arr[], int n, int x, int cap, int pos)
 {
+	//If size of array we give as an input is equal to capacity of array so we can not insert element. 
 	if(n == cap)
 		return n;
+	
 	int idx = pos - 1;
+	//We start from end and go through (pos-1)
 	for(int i = n - 1; i >= idx; i--)
 	{
+		//We shift every element forward
 		arr[i + 1] = arr[i];
 	}
+	//Insert x at (pos-1)
 	arr[idx] = x;
+	
+	//return new n
 	return n + 1;
 }
 
