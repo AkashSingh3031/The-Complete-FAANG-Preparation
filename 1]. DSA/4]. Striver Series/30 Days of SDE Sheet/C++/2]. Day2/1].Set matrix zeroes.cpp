d66e88ec5dce77ff42148cbdbf84ffb008c7d265
Suniti <72435330+suniti0804@users.noreply.github.com>
2021-10-02 15:28:19 +0530

    Update 1].Set matrix zeroes.cpp

--- 1]. DSA/4]. Striver Series/30 Days of SDE Sheet/C++/2]. Day2/1].Set matrix zeroes.cpp
+++ 1]. DSA/4]. Striver Series/30 Days of SDE Sheet/C++/2]. Day2/1].Set matrix zeroes.cpp
@@ -1,16 +1,12 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 
- // } Driver Code Ends
-
-
-
 class Solution
 {   
     public:
     //Function to modify the matrix such that if a matrix cell matrix[i][j]
-    //is 1 then all the cells in its ith row and jth column will become 1.
-    void booleanMatrix(vector<vector<int> > &matrix)
+    //is 0 then all the cells in its ith row and jth column will become 0.
+    void booleanMatrix(vector<vector<int>> &matrix)
     {
         int m = matrix.size();
         int n = matrix[0].size();
@@ -21,29 +17,32 @@ class Solution
             {
                 if(matrix[i][j] == 0) 
                 {
-                    if(i == 0) row = true;
-                    if(j == 0) col = true;
+                    if(i == 0) 
+                        row = true;
+                    if(j == 0) 
+                        col = true;
                     matrix[0][j] = matrix[i][0] = 0;
                 }
             }
         }
+        
         for(int i = 1; i < m; i++)
         {
             for(int j = 1; j < n; j++)
             {
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) 
-                  matrix[i][j] = 0;
+                    matrix[i][j] = 0;
             }
         }
         if(col)
         {
             for(int i = 0; i < matrix.size(); i++) 
-              matrix[i][0] = 0;
-        }
+                matrix[i][0] = 0;
+        }     
         if(row)
         {
             for(int j = 0; j < matrix[0].size(); j++) 
-              matrix[0][j] = 0;
+                matrix[0][j] = 0;
         }
     }
 };
@@ -82,6 +81,3 @@ int main() {
     }
     return 0;
 }
-
-
-  // } Driver Code Ends
