4fdd3a31eafc40723c48d356d02e541fe6c48e36
Anubhav Shukla <86482181+Uchiha-Itachi0@users.noreply.github.com>
2021-10-20 19:57:28 +0530

    Adding solution to DSA Love Babar in python

---
+++ 1]. DSA/3]. 450 DSA by ( Love Babbar Bhaiya )/Python/01]. Array/01_)_Reverse_Array.py
@@ -0,0 +1,10 @@
+def reverse(arr):
+    """
+    This program simply reverse the given array
+    Time Complexity : O(n)
+    Space Complexity : O(1)
+    """
+    return arr[::-1]
+
+
+print(reverse([1, 2, 3, 4]))
