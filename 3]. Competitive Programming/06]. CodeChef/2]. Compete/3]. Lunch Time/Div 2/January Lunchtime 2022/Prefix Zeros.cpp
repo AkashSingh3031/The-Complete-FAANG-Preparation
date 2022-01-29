30f7d0d14dd6de3fe72dbce1ba6a1451902ddd4a
avnoor-488 <avnoorsingh488@gmail.com>
2022-01-29 23:32:21 +0530

    Added January Lunchtime 2022

---
+++ 3]. Competitive Programming/06]. CodeChef/2]. Compete/3]. Lunch Time/Div 2/January Lunchtime 2022/Prefix Zeros.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define all(cont) cont.begin(), cont.end()
+#define vll vector<ll>
+#define pll pair<ll, ll>
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vector<string>> vss;
+// typedef map<int,int>
+
+void readVec(vi &vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+}
+
+
+void printVec(vi vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << vec[i] << " ";
+    cout << endl;
+}
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+void solve()
+{
+    ll n, k; 
+    cin >> n >> k;
+    string s; 
+    cin >> s;
+    ll start = 0,end = n - 1, ans = 0;
+    
+    while (start <= end)
+    {
+        ll p = start + (end - start) / 2;
+
+        ll x = 0;
+        for (ll i = p; i >= 0; i--) {
+            ll z = ((s[i] - '0') + x) % 10;
+            if (z == 0) continue;
+            x += 10 - z;
+        }
+
+
+        if (x > k) {
+            end = p - 1;
+        }
+        else {
+            ans = p + 1;
+            start = p + 1;
+        }
+    }
+    cout << ans << endl;
+
+}
+
+
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
---
+++ 3]. Competitive Programming/06]. CodeChef/2]. Compete/3]. Lunch Time/Div 2/January Lunchtime 2022/Subarray Permutations.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define sec second
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define all(cont) cont.begin(), cont.end()
+#define vll vector<ll>
+#define pll pair<ll, ll>
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vv;
+typedef vector<vector<string>> vss;
+
+
+void readVec(vi &vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+}
+
+
+void printVec(vi vec, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << vec[i] << " ";
+    cout << endl;
+}
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    if (n == 1 and k == 1)cout << 1 << endl;
+    else
+    {
+        if (k < 2 or k > n)cout << -1;
+        else
+        {
+            int count = 0;
+            for (int i = 0; i < k - 1; i++)
+            {
+                cout << (i + 1) << " ";
+                count = i;
+            }
+            for (int i = 0; i < n - count - 1; i++)
+            {
+                cout << (n - i) << " ";
+            }
+        }
+        cout << endl;
+    }
+
+}
+
+
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
