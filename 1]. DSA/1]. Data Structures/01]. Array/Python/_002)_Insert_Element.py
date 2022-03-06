17bfb95706befb7cf439d5953342fcba68035007
Guilherme Figueirêdo <97318219+oguilhermefigueiredo@users.noreply.github.com>
2022-03-06 11:25:54 -0300

    Update _002)_Insert_Element.py

--- 1]. DSA/1]. Data Structures/01]. Array/Python/_002)_Insert_Element.py
+++ 1]. DSA/1]. Data Structures/01]. Array/Python/_002)_Insert_Element.py
@@ -1,20 +1,19 @@
+
 """
 Function to insert a new element at a given position
 
 Takes the position of an array and an element as arguments
 """
 
-
 def insert(arr, index, successor):
-    
     for index in range(index, len(arr)):
         arr[index], successor = successor, arr[index]
 
     arr.append(successor)
-    
 
 arr = [1, 2, 3, 5]
 print("Array before insertion:", arr)
-
-insert(arr, 1, -4)
+# OUTPUT: [1, 2, 3, 5]
+insert(arr, 3, -4) 
 print("Array after insertion:", arr)
+# OUTPUT: [1, 2, 3, -4, 5]
