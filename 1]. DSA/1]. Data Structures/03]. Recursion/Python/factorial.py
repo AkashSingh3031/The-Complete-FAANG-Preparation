9a03df8cfceff97dda83d475c434373ddfc69988
Guilherme Figueirêdo <97318219+oguilhermefigueiredo@users.noreply.github.com>
2022-03-12 14:26:58 -0300

    Calculating factorial with recursion

--- 1]. DSA/1]. Data Structures/03]. Recursion/Python/factorial.py
+++ 1]. DSA/1]. Data Structures/03]. Recursion/Python/factorial.py
@@ -1,7 +1,15 @@
+# Calculating the factorial of an given argument "n"
+# Mathematical formula:
+# n! = 1.2.3...(n-2).(n-1).n
 def factorial(n):
-    if n==1:
+    if n <= 1:
         return 1
-    else:
-        return n*factorial(n-1)
+
+    return n*factorial(n-1)
 
 print(factorial(5))
+# OUTPUT: 120
+print(factorial(0))
+# OUTPUT 1
+print(factorial(1))
+# OUTPUT:1
