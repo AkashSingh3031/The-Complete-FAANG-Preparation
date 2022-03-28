c2c2c776541f7c812a6305d9b1a462220a240111
Akash Singh <60146338+AkashSingh3031@users.noreply.github.com>
2022-03-28 10:57:46 +0530

    Update _04)_Sort_Array_without_Algo.cpp

--- 1]. DSA/3]. 450 DSA by ( Love Babbar Bhaiya )/C++/01]. Array/_04)_Sort_Array_without_Algo.cpp
+++ 1]. DSA/3]. 450 DSA by ( Love Babbar Bhaiya )/C++/01]. Array/_04)_Sort_Array_without_Algo.cpp
@@ -1,3 +1,27 @@
+/*
+Example 1:
+    Input: 
+        N = 5
+        arr[]= {0 2 1 2 0}
+    Output:
+        0 0 1 2 2
+    
+  Explanation:
+    0s 1s and 2s are segregated 
+    into ascending order.
+
+Example 2:
+    Input: 
+        N = 3
+        arr[] = {0 1 0}
+    Output:
+        0 0 1
+  
+  Explanation:
+    0s 1s and 2s are segregated 
+    into ascending order.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
