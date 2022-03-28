2974cd16a15c54192ce8836143f92ab115dc1d9f
Akash Singh <60146338+AkashSingh3031@users.noreply.github.com>
2022-03-28 10:38:10 +0530

    Update and rename _01)_Reverse_String.cpp to 01]. Reverse String.cpp

--- 1]. DSA/3]. 450 DSA by ( Love Babbar Bhaiya )/C++/01]. Array/01]. Reverse String.cpp
+++ 1]. DSA/3]. 450 DSA by ( Love Babbar Bhaiya )/C++/01]. Array/01]. Reverse String.cpp
@@ -1,22 +1,16 @@
+/*
+Example 1:
+	Input: s = Geeks
+	Output: skeeG
+Example 2:
+	Input: s = for
+	Output: rof
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
-string reverseWord(string str);
-
-int main() {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-        string s;
-        cin >> s;
-        cout << reverseWord(s) << endl;
-	}
-	return 0;
-}
-
 string reverseWord(string str){
-    
   //Your code here
   int low=0, high=str.length()-1;
   
@@ -29,3 +23,14 @@ string reverseWord(string str){
   return str;
 }
 
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+        string s;
+        cin >> s;
+        cout << reverseWord(s) << endl;
+	}
+	return 0;
+}
