5e36ae484eb16b823dff05402b8a32565cf8a264
Akash Singh <60146338+AkashSingh3031@users.noreply.github.com>
2022-03-28 10:46:30 +0530

    Update _03)_Kth_Max_Min_Element.cpp

--- 1]. DSA/3]. 450 DSA by ( Love Babbar Bhaiya )/C++/01]. Array/_03)_Kth_Max_Min_Element.cpp
+++ 1]. DSA/3]. 450 DSA by ( Love Babbar Bhaiya )/C++/01]. Array/_03)_Kth_Max_Min_Element.cpp
@@ -1,3 +1,27 @@
+/*
+Example 1:
+    Input:
+        N = 6
+        arr[] = 7 10 4 3 20 15
+        K = 3
+    Output : 7
+    
+  Explanation :
+    3rd smallest element in the given 
+    array is 7.
+
+Example 2:
+    Input:
+        N = 5
+        arr[] = 7 10 4 20 15
+        K = 4
+    Output : 15
+    
+  Explanation :
+    4th smallest element in the given 
+    array is 15.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -69,8 +93,6 @@ class Solution{
         return i;
     }
 };
-
-
  
 int main()
 {
