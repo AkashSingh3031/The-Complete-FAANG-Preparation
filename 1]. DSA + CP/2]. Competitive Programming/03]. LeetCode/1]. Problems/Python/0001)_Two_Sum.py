f17b2ddfd6dcca2fc5e3830244fd3f3a10cda586
Shashank Raj <shashank1_theone@yahoo.co.in>
2022-06-06 10:05:10 +0530

    Fixed the solution as original was incorrect.

--- 1]. DSA + CP/2]. Competitive Programming/03]. LeetCode/1]. Problems/Python/0001)_Two_Sum.py
+++ 1]. DSA + CP/2]. Competitive Programming/03]. LeetCode/1]. Problems/Python/0001)_Two_Sum.py
@@ -1,9 +1,26 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
+    def __init__(self,nums: list[int], target: int):
+        self.nums = nums
+        self.target = target
+
+    # def twoSum(self) 
+    #     num_to_index = {}
         
-        for i, num in enumerate(nums):
-            if target-num in num_to_index:
-                return [num_to_index[target-num], i]
-            num_to_index[num] = i
+    #     for i, num in enumerate(self.nums):
+    #         if self.target-num in num_to_index:
+    #             return [num_to_index[self.target-num], i]
+    #         num_to_index[num] = i
+    #     return []
+    
+    def twoNumberSum(self) -> list[int]:
+        for i in range(len(self.nums)-1):
+            firstNum = self.nums[i]
+            for j in range(i+1,len(self.nums)):
+                secondNum = self.nums[j]
+                if firstNum+secondNum == self.target:
+                    return [firstNum,secondNum]
         return []
+
+sol1 = Solution([2,5,3,6,9,-2], 8)
+# print(sol1.twoSum())
+print(sol1.twoNumberSum())
