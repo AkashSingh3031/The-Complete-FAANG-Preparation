c37f1c1e3f240453b08ceddf0084ce32acc53cf8
Shubhanshu Rao <87806305+shubhanshurav@users.noreply.github.com>
2022-12-21 12:26:33 +0530

    Update Repo

---
+++ 1]. DSA + CP/1]. DSA/1]. Data Structures/06]. Linked List/Delete-Node-in-Linked-List.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+               
+        node->val=node->next->val;
+		node->next=node->next->next;
+        
+    }
+};
