6575ea04eaa80ec332833a9e8449c00ff3c65bf1
Shubhanshu Rao <87806305+shubhanshurav@users.noreply.github.com>
2022-12-30 17:54:49 +0530

    Update repo by Shubhanshu

--- 1]. DSA + CP/1]. DSA/1]. Data Structures/13]. Binary Search Tree/C++/_11)_Insert-in-BST.cpp
+++ 1]. DSA + CP/1]. DSA/1]. Data Structures/13]. Binary Search Tree/C++/_11)_Insert-in-BST.cpp
@@ -12,24 +12,6 @@
 class Solution {
 public:
     
-/*<............... Recursive Solution .......................>
-    
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-                
-        if(root == NULL)
-            return new TreeNode(val);
-        
-        if(root->val > val){
-            root->left = insertIntoBST(root->left,val);
-        }else if(root->val < val){
-            root->right = insertIntoBST(root->right,val);
-        }
-        
-        return root;
-        
-    }
-*/    
-    
 //<............... Iterative Solution .......................>
     
  TreeNode* insertIntoBST(TreeNode* root, int val) {
