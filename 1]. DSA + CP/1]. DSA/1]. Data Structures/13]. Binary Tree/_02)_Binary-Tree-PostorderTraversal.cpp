a9c85f6a79237e69e21b8a1b2eb21bcf45a04cd7
Shubhanshu Rao <87806305+shubhanshurav@users.noreply.github.com>
2023-01-05 10:44:22 +0530

    Update repo

--- 1]. DSA + CP/1]. DSA/1]. Data Structures/13]. Binary Tree/_02)_Binary-Tree-PostorderTraversal.cpp
+++ 1]. DSA + CP/1]. DSA/1]. Data Structures/13]. Binary Tree/_02)_Binary-Tree-PostorderTraversal.cpp
@@ -12,35 +12,16 @@
 class Solution {
 public:
     
-    /* <............... Approach - 01 ..........................>
+    void postorder(TreeNode *root, vector<int> &ans){
+
+          if(root == NULL) return;
+
+          postorder(root->left,ans);
+          postorder(root->right,ans);
+          ans.push_back(root->val);         
+    }
     
-    vector<int> ans;
     vector<int> postorderTraversal(TreeNode* root) {
-        
-        if(root == NULL) return {};
-        
-        postorderTraversal(root->left);
-        postorderTraversal(root->right);
-        ans.push_back(root->val);
-        
-        return ans;
-      
-     */
-    
-  //<................. Approach - 02 ................................>  
-    
-     void postorder(TreeNode *root, vector<int> &ans){
-         
-         if(root == NULL )
-             return;
-         
-         postorder(root->left,ans);
-         postorder(root->right,ans);
-         ans.push_back(root->val);
-         
-     }
-    
-     vector<int> postorderTraversal(TreeNode* root) {
          
          vector<int> ans;
          postorder(root,ans);
