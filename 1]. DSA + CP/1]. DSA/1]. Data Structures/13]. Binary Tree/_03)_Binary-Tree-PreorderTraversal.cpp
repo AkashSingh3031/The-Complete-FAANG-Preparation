622e5f420d879ed20a3a1686ebb0771dc606f054
Shubhanshu Rao <87806305+shubhanshurav@users.noreply.github.com>
2023-01-05 10:49:38 +0530

    Update repo

--- 1]. DSA + CP/1]. DSA/1]. Data Structures/13]. Binary Tree/_03)_Binary-Tree-PreorderTraversal.cpp
+++ 1]. DSA + CP/1]. DSA/1]. Data Structures/13]. Binary Tree/_03)_Binary-Tree-PreorderTraversal.cpp
@@ -11,32 +11,7 @@
  */
 class Solution {
 public:
-    
-/*<........................ Approach - 01 .............................>
-    
-    void preorder(TreeNode *root, vector<int> &ans){
-        
-         if(root == NULL) 
-            return;
-        
-        ans.push_back(root->val);
-        preorder(root->left,ans);
-        preorder(root->right,ans);
-        
-    }
-
-    vector<int> preorderTraversal(TreeNode* root){
-        
-        vector<int> ans;
-    
-        preorder(root,ans);
-        return ans;
- */
- 
-        
-//<........................ Approach - 02 .............................>
-  
-        vector<int> ans;
+    vector<int> ans;
     vector<int> preorderTraversal(TreeNode* root){
         
         if(root == NULL) return {};
