6f0e446e04a5d9582722dc4c9efb78ca2f1d30d7
Pratyush Tyagi <86615560+Pratyush0612@users.noreply.github.com>
2023-01-14 14:37:16 +0530

    Update 01) Binary Search.cpp

--- 1]. DSA + CP/1]. DSA/2]. Algorithms/01]. Searching Algorithms/C++/01) Binary Search.cpp
+++ 1]. DSA + CP/1]. DSA/2]. Algorithms/01]. Searching Algorithms/C++/01) Binary Search.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int binarySearch(int arr[],int n,int key){
+int binarySearch(int arr[], int n, int key){
 int s = 0 ,e = n-1;
-while(s<=e){
+while(s <= e){
 	int mid = s + (e-s)/2;
-if(arr[mid]==key){
+if(arr[mid] == key){
 	return mid;
 }
-if(key>arr[mid]){
+if(key > arr[mid]){
 	s = mid+1;
 }
 else{
@@ -17,13 +17,13 @@ mid = s + (e-s)/2;
 }
 return -1;
 }
-int main(){
+int main() {
 
-int arr[]={1,2,4,5,7,9,11};
-int n=7;
-int ans=binarySearch(arr,7,2);
-if(ans==-1)
-	cout<<"Not Found";
+int arr[] = {1,2,4,5,7,9,11};
+int n = 7;
+int ans = binarySearch(arr,7,2);
+if(ans == -1)
+	cout<<"Not Found ";
 else
-	cout<<"Element found at index "<<ans;
+	cout<<" Element found at index " << ans;
 }
