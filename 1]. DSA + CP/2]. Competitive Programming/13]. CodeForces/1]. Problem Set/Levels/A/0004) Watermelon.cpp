4d946aae205f6635a111bdf757cd204a42182840
Pratyush Tyagi <86615560+Pratyush0612@users.noreply.github.com>
2023-01-14 14:37:23 +0530

    Update 0004) Watermelon.cpp

--- 1]. DSA + CP/2]. Competitive Programming/13]. CodeForces/1]. Problem Set/Levels/A/0004) Watermelon.cpp
+++ 1]. DSA + CP/2]. Competitive Programming/13]. CodeForces/1]. Problem Set/Levels/A/0004) Watermelon.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std ;
 int main(){
     int w;
-    cin>> w;
-    if(w%2==0 && w!=2){
+    cin >> w;
+    if(w%2 == 0 && w!=2){
         cout<< "YES"<<endl;
     }
     else{
